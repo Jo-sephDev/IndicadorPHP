a629131e9a5f9424c84329a8909e33504c88c39d
JUAN J RIOS <juangaming070@gmail.com>
2025-04-01 19:34:33 -0500

    Entidades Crud

---
+++ Model/Fuente.php
@@ -0,0 +1,30 @@
+<?php
+
+class Fuente
+{
+    private $id;
+    private $nombre;
+    
+    function __construct($id, $nombre = []) {
+        $this->id = $id;
+        $this->nombre = $nombre;
+    }
+
+    public function __set($nombre, $valor) {
+        $this->propiedades[$nombre] = $valor;
+    }
+
+    public function __get($nombre) {
+        if (array_key_exists($nombre, $this->propiedades)) {
+            return $this->propiedades[$nombre];
+        }
+        trigger_error("Propiedad no definida: " . $nombre, E_USER_NOTICE);
+        return null;
+    }
+
+    // Método para obtener todas las propiedades
+    public function obtenerPropiedades() {
+        return $this->propiedades;
+    }
+}
+?> 
